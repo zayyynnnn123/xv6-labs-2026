15298b02080e5cc0fc252680e47ecfdc90e87e2d
zayyynnnn123 <zainaleem42@gmail.com>
2026-09-15 12:23:27 +0500

    added the second exercise for assignment

--- user/find.c
+++ user/find.c
@@ -21,6 +21,44 @@ fmtname(char *path)
   memset(buf + strlen(p), 0, DIRSIZ - strlen(p));
   return buf;
 }
+int matchhere(char *, char *);
+int matchstar(int, char *, char *);
+
+int
+match(char *re, char *text)
+{
+  if (re[0] == '^')
+    return matchhere(re + 1, text);
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text++ != '\0');
+  return 0;
+}
+
+int
+matchhere(char *re, char *text)
+{
+  if (re[0] == '\0')
+    return 1;
+  if (re[1] == '*')
+    return matchstar(re[0], re + 2, text);
+  if (re[0] == '$' && re[1] == '\0')
+    return *text == '\0';
+  if (*text != '\0' && (re[0] == '.' || re[0] == *text))
+    return matchhere(re + 1, text + 1);
+  return 0;
+}
+
+int
+matchstar(int c, char *re, char *text)
+{
+  do {
+    if (matchhere(re, text))
+      return 1;
+  } while (*text != '\0' && (*text++ == c || c == '.'));
+  return 0;
+}
 
 // fork/exec "cmd cmd_argv... file"
 void
@@ -73,7 +111,7 @@ find(char *path, char *name, char *cmd, char **cmd_argv, int cmd_argc)
 
   switch (st.type) {
   case T_FILE:
-    if (strcmp(fmtname(path), name) == 0) {
+    if (match(name, fmtname(path))) {
       if (cmd)
         run_exec(cmd, cmd_argv, cmd_argc, path);
       else
