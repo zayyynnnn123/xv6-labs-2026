d8a4be7a8c76ccfc3c29c16c4b2ea7d50e36d73a
zayyynnnn123 <zainaleem42@gmail.com>
2026-09-15 10:50:11 +0500

    implemented the final Problem set 1 exercise

--- user/find.c
+++ user/find.c
@@ -1,21 +1,59 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
-#include "kernel/fs.h"
+#include "kernel/param.h"
 #include "user/user.h"
+#include "kernel/fs.h"
 
-// extract just the filename component from a full path
 char *
-basename(char *path)
+fmtname(char *path)
 {
+  static char buf[DIRSIZ + 1];
   char *p;
+
+  // Find first character after last slash.
   for (p = path + strlen(path); p >= path && *p != '/'; p--)
     ;
   p++;
-  return p;
+
+  if (strlen(p) >= DIRSIZ)
+    return p;
+  memmove(buf, p, strlen(p));
+  memset(buf + strlen(p), 0, DIRSIZ - strlen(p));
+  return buf;
+}
+
+// fork/exec "cmd cmd_argv... file"
+void
+run_exec(char *cmd, char **cmd_argv, int cmd_argc, char *file)
+{
+  char *argv[MAXARG];
+  int i, pid;
+
+  if (cmd_argc + 2 > MAXARG) {
+    fprintf(2, "find: too many arguments\n");
+    return;
+  }
+
+  for (i = 0; i < cmd_argc; i++)
+    argv[i] = cmd_argv[i];
+  argv[cmd_argc] = file;
+  argv[cmd_argc + 1] = 0;
+
+  pid = fork();
+  if (pid < 0) {
+    fprintf(2, "find: fork failed\n");
+    return;
+  }
+  if (pid == 0) {
+    exec(cmd, argv);
+    fprintf(2, "find: exec %s failed\n", cmd);
+    exit(1);
+  }
+  wait(0);
 }
 
 void
-find(char *path, char *target)
+find(char *path, char *name, char *cmd, char **cmd_argv, int cmd_argc)
 {
   char buf[512], *p;
   int fd;
@@ -35,8 +73,12 @@ find(char *path, char *target)
 
   switch (st.type) {
   case T_FILE:
-    if (strcmp(basename(path), target) == 0)
-      printf("%s\n", path);
+    if (strcmp(fmtname(path), name) == 0) {
+      if (cmd)
+        run_exec(cmd, cmd_argv, cmd_argc, path);
+      else
+        printf("%s\n", path);
+    }
     break;
 
   case T_DIR:
@@ -47,32 +89,54 @@ find(char *path, char *target)
     strcpy(buf, path);
     p = buf + strlen(buf);
     *p++ = '/';
-
     while (read(fd, &de, sizeof(de)) == sizeof(de)) {
       if (de.inum == 0)
         continue;
-      if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
-        continue;
-
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
-
-      find(buf, target);
+      if (stat(buf, &st) < 0) {
+        printf("find: cannot stat %s\n", buf);
+        continue;
+      }
+      if (strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+        continue;
+      find(buf, name, cmd, cmd_argv, cmd_argc);
     }
     break;
   }
-
   close(fd);
 }
 
 int
 main(int argc, char *argv[])
 {
-  if (argc != 3) {
-    fprintf(2, "Usage: find dir name\n");
+  char *path, *name, *cmd = 0;
+  char *cmd_argv[MAXARG];
+  int cmd_argc = 0;
+  int i;
+
+  if (argc < 3) {
+    fprintf(2, "usage: find path name [-exec cmd args...]\n");
     exit(1);
   }
 
-  find(argv[1], argv[2]);
+  path = argv[1];
+  name = argv[2];
+
+  if (argc > 3) {
+    if (strcmp(argv[3], "-exec") != 0) {
+      fprintf(2, "usage: find path name [-exec cmd args...]\n");
+      exit(1);
+    }
+    if (argc < 5) {
+      fprintf(2, "find: -exec requires a command\n");
+      exit(1);
+    }
+    cmd = argv[4];
+    for (i = 4; i < argc && cmd_argc < MAXARG - 2; i++)
+      cmd_argv[cmd_argc++] = argv[i];
+  }
+
+  find(path, name, cmd, cmd_argv, cmd_argc);
   exit(0);
 }
